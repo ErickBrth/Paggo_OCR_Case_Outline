209ebb86ff8d03310ffa40fcb7ad7c6238ad6230
ErickBrth <erickaraken@gmail.com>
2025-05-02 22:57:13 -0300

    fix: CORS url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://paggo-ocr-case-outline.vercel.app',
+    ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   await app.listen(3001);
 }
 void bootstrap();
